bae390431625e9c06be1a9e66e7b1085fd092230
Jonathan <cursed_ally@hotmail.com>
2013-02-22 12:39:36 +0100

    commit de prueba
    fichero index de webalmacen

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+$GLOBALS['startTime'] = microtime(true);
+$mant = false;
+$allowIp = "";
+
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+   $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} 
+elseif (isset($_SERVER['HTTP_VIA'])) {
+   $ip = $_SERVER['HTTP_VIA'];
+} 
+elseif (isset($_SERVER['REMOTE_ADDR'])) {
+   $ip = $_SERVER['REMOTE_ADDR'];
+}
+else { 
+   $ip = "unknown";
+}
+
+if (!$mant Or $ip == $allowIp)
+{
+	// Specify your config section here
+
+	include 'application/bootstrap.php';
+	$bootstrap = new Bootstrap();
+	$bootstrap->runApp();
+}
+else {
+	// Mantenimiento
+	header("HTTP/1.1 503 Service Unavailable");
+	header("Retry-After: 3600");
+	include("mantenimiento.html");
+}
+
+?>
